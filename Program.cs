420ddbff95344e3749834135cf1780fe9f647383
sajmitch <144658831+sajmitch@users.noreply.github.com>
2023-09-24 20:47:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Zadejte kladné číslo N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("Fizz Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
