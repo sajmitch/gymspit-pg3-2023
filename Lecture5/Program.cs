48195570764c46de7f098cfb9cb7bef588097a24
sajmitch <144658831+sajmitch@users.noreply.github.com>
2023-10-16 09:41:25 +0200

    Add files via upload

---
+++ Lecture5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+int sifra;
+string text;
+int key;
+Console.WriteLine("Napiš text");
+text = Console.ReadLine();
+Console.WriteLine("O kolik míst chcete posunout?");
+int.TryParse(Console.ReadLine(), out key);
+char[] textArray = text.ToCharArray();
+for (int i = 0; i < textArray.Length; i++)
+{
+    sifra = textArray[i];
+    sifra = sifra + key;
+    textArray[i] = (char)sifra;
+}
+for  (int i = 0;i < textArray.Length; i++)
+{
+    Console.WriteLine(textArray[i]);
+}
