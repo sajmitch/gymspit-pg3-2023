a8fc4b457ededf316f37b118e3ff725317212d3d
sajmitch <144658831+sajmitch@users.noreply.github.com>
2024-04-22 09:31:41 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,53 @@
-﻿int delka;
-Console.WriteLine("Zadej délku pole");
-int.TryParse(Console.ReadLine(), out delka);
-Console.WriteLine("Délka pole: {0}", delka);
+﻿using Lecture18;
 
-bool[] a = new bool[delka];
+public class Program
 
-for (int i = 0; a.Length > i; i++)
 {
-    a[i] = true;
-}
 
-for (int i = 2; i < a.Length; i++)
-{
-    if (a[i])
+    private static Random random = new Random();
+
+    public static void Main()
+
     {
-        for (int x = i * 2; x < a.Length; x += i)
+
+        Character c3PO = new NPC(GetRandomName(), 15, 6, 4, random);
+
+        Character r2D2 = new NPC(GetRandomName(), 10, 7, 5, random);
+
+        Character luke = new Player(GetRandomName(), 20, 4, 4, Console.In, Console.Out);
+
+        Game game = new Game(c3PO, r2D2, new Die(random, 6));
+
+        game.Run(Console.Out);
+
+        Console.WriteLine();
+
+        Game game2 = new Game(c3PO, luke, new Die(random, 6));
+
+        game2.Run(Console.Out);
+
+        Console.WriteLine();
+
+    }
+
+    private static string GetRandomName()
+
+    {
+
+        // Seznam možných jmen
+
+        string[] names = new string[]
+
         {
-            a[x] = false;
-        }
-    }
-       
 
-}
+            "Alex", "Maria", "Ethan", "Olivia", "Noah", "Ava", "Liam", "Sophia", "Mason", "Isabella", "Jacob", "Mia", "William", "Amelia", "James", "Charlotte", "Benjamin", "Harper", "Lucas", "Emily", "Henry", "Madison", "Michael", "Scarlett", "Daniel", "Victoria", "Jack", "Aiden", "Matthew", "Grace", "Samuel", "Chloe", "David", "Lily", "Joseph", "Ruby", "Carter", "Ella", "Owen", "Abigail", "Wyatt", "Zoe", "John", "Layla", "Luke", "Natalie", "Jayden", "Aurora", "Dylan", "Savannah", "Grayson", "Audrey", "Levi", "Brooklyn", "Isaac", "Lucy", "Gabriel", "Emma", "Julian", "Bella", "Mateo", "Alexis", "Anthony", "Ellie", "Jaxon", "Hannah", "Lincoln", "Addison", "Joshua", "Evelyn", "Christopher", "Harper", "Andrew", "Brielle", "Theodore", "Hailey", "Caleb", "Leah", "Ryan", "Nora", "Asher", "Skylar", "Nathan", "Sarah", "Thomas", "Aria", "Leo", "Penelope", "Isaiah", "Ariana", "Charles", "Elena", "Josiah", "Gabriella", "Hudson", "Paisley", "Christian", "Audrey", "Hunter", "Zoe", "Connor", "Stella", "Eli", "Maya", "Ezra", "Camila", "Aaron", "Addison", "Landon", "Avery", "Adrian", "Kinsley", "Jonathan", "Piper", "Nolan", "Arianna", "Jeremiah", "Maria", "Easton", "Angelina", "Elias", "Raelynn", "Colton", "Ivy", "Cameron", "Kennedy", "Carson", "Madelyn", "Robert", "Brianna", "Angel", "Kylie", "Nicholas", "Mackenzie", "Dominic", "Kaitlyn", "Jace", "Ashlyn", "Greyson", "Cora", "Parker", "Adriana", "Mateo", "Reagan", "Jason", "Alison", "Santiago", "Mariana", "Axel", "Molly"
 
-for (int i = 0; a.Length > i; i++)
-{
-    if (a[i])
-    {
-        Console.WriteLine(i);
+        };
+
+        // Náhodný výběr jména z pole
+
+        return names[random.Next(names.Length)];
+
     }
+
 }
